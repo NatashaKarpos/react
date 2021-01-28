71f9f965fd605b2a30b893138e16f8ba041c8096
NatashaKarpos <karpova2308@gmail.com>
2021-01-28 16:06:00 +0300

    homework2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
-import logo from './logo.svg';
+import Header from './components/header/index';
+import Layout from './components/layout/index';
+import Footer from './components/footer/index';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-          <h1>
-            Hello, React!
-          </h1>
-      </header>
+    <div>
+      <Header 
+        title="title"
+        descr="descr"
+      />
+      <Layout 
+        id="1"
+        title="title"
+        descr="descr"
+        urlBg="/static/media/bg1.339e2b02.jpg"
+        colorBg=''
+      />
+      <Layout 
+        id="1"
+        title="title"
+        descr="descr"
+        urlBg=''
+        colorBg="#555"
+      />
+      <Layout 
+        id="1"
+        title="title"
+        descr="descr"
+        urlBg="/static/media/bg3.59f1c1e9.jpg"
+        colorBg=''
+      />
+      <Footer />
     </div>
   );
 }
